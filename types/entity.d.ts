a42831db9283a3813614e6a4295811ea08ffc2db
Steven Blom <atomicblom@binaryvibrance.net>
2018-10-28 01:40:44 +1030

    Deprecate internal fields being exposed

--- types/entity.d.ts
+++ types/entity.d.ts
@@ -1,6 +1,15 @@
 declare interface IEntityObject {
     id: string;
+    /**
+     * The identifier of the entity, e.g., "minecraft:sheep", or "minecraft:pumpkin_seeds"
+     * @deprecated this is not meant to be public, but it is currently the only way to get the identifier of an item
+     */
     __identifier__: string;
+
+    /**
+     * The type of the entity
+     * @deprecated this is not meant to be public, but it is currently the only way to determine if something is an item
+     */
     __type__: EntityType;
 }
 
