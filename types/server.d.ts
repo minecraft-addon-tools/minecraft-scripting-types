5d8ee06b3be36e20c8c531ad418ce8e250abc2ac
abc-55 <abc55@outlook.cz>
2018-10-28 22:12:35 +0100

    made IVanillaServerSystem the default value for TSystem type parameter

--- types/server.d.ts
+++ types/server.d.ts
@@ -1,6 +1,6 @@
 declare const server: IServer;
 
 declare interface IServer {
-    registerSystem<TSystem extends ISystemBase>(majorVersion: number, minorVersion: number): TSystem;
+    registerSystem<TSystem extends ISystemBase = IVanillaServerSystem>(majorVersion: number, minorVersion: number): TSystem;
     log(message: string): void;
 }
