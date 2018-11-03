78dc94994de76f298650279f0caa6be102f419fa
Steven Blom <atomicblom@binaryvibrance.net>
2018-11-03 15:18:21 +1030

    Prefix all particles with minecraft:

--- types/particle_effects.d.ts
+++ types/particle_effects.d.ts
@@ -4,178 +4,178 @@ declare const enum MinecraftParticleEffect {
     /**
      * Beacon effects
      */
-    MobSpellAmbient = "mobspellambient",
+    MobSpellAmbient = "minecraft:mobspellambient",
     /**
      * Attacking a villager in a village
      */
-    VillagerAngry = "villagerangry",
+    VillagerAngry = "minecraft:villagerangry",
     /**
      * Breaking blocks, sprinting, iron golems walking
      */
-    BlockBreak = "blockbreak",
+    BlockBreak = "minecraft:blockbreak",
     /**
      * Breaking armor stands, falling
      */
-    BlockDust = "blockdust",
+    BlockDust = "minecraft:blockdust",
     /**
      * Entities in water, guardian laser beams, fishing
      */
-    Bubble = "bubble",
+    Bubble = "minecraft:bubble",
     /**
      * After jumping into water while on fire
      */
-    Evaporation = "evaporation",
+    Evaporation = "minecraft:evaporation",
     /**
      * Critical hits, bows, evoker fangs
      */
-    CriticalHit = "crit",
+    CriticalHit = "minecraft:crit",
     /**
      * 	An ender dragon's breath and dragon fireballs
      */
-    DragonBreath = "dragonbreath",
+    DragonBreath = "minecraft:dragonbreath",
     /**
      * Dripping lava through blocks
      */
-    DripLava = "driplava",
+    DripLava = "minecraft:driplava",
     /**
      * 	Dripping water through blocks, wet sponges, leaves when raining
      */
-    DripWater = "dripwater",
+    DripWater = "minecraft:dripwater",
     /**
      * Redstone ore, powered redstone dust, redstone torches, powered redstone repeaters
      */
-    RedstoneDust = "reddust",
+    RedstoneDust = "minecraft:reddust",
     /**
      * Splash potions, lingering potions, bottles o' enchanting, evokers.
      */
-    Spell = "spell",
+    Spell = "minecraft:spell",
     /**
      * Elder Gardians
      * note: wiki has a question mark
      */
-    MobAppearance = "mobappearance",
+    MobAppearance = "minecraft:mobappearance",
     /**
      * From bookshelves near an enchanting table.
      */
-    EnchantingTable = "enchantingtable",
+    EnchantingTable = "minecraft:enchantingtable",
     /**
      * End rods, shulker bullets.
      */
-    EndRod = "endrod",
+    EndRod = "minecraft:endrod",
     /**
      * 	Status effects, lingering potions, tipped arrows, trading, withered armor (linger potion particles decrease when the "minimal" particle setting is used).
      */
-    MobSpell = "mobspell",
+    MobSpell = "minecraft:mobspell",
     /**
      * Explosions, ghast fireballs, wither skulls, ender dragon death, shearing mooshrooms.
      */
-    LargeExplosion = "largeexplode",
+    LargeExplosion = "minecraft:largeexplode",
     /**
      * Floating sand, gravel, concrete powder, and anvils.
      */
-    FallingDust = "fallingdust",
+    FallingDust = "minecraft:fallingdust",
     /**
      * Firework rocket trail and explosion (trail is not shown when the "minimal" particle setting is used), when dolphins track shipwrecks and underwater ruins.
      */
-    FireworksSpark = "fireworksspark",
+    FireworksSpark = "minecraft:fireworksspark",
     /**
      * Fishing
      */
-    WaterWake = "waterwake",
+    WaterWake = "minecraft:waterwake",
     /**
      * 	Torches, furnaces, magma cubes, spawners.
      */
-    Flame = "flame",
+    Flame = "minecraft:flame",
     /**
      * 	Bone mealing a crop, trading with villagers, feeding baby animals, walking or jumping on turtle eggs.
      */
-    VillagerHappy = "villagerhappy",
+    VillagerHappy = "minecraft:villagerhappy",
     /**
      * Breeding and taming animals.
      */
-    Heart = "heart",
+    Heart = "minecraft:heart",
     /**
      * Explosions, ender dragon death.
      */
-    HugeExplosion = "hugeexplosion",
+    HugeExplosion = "minecraft:hugeexplosion",
     /**
      * Instant health/damage splash and lingering potions, spectral arrows.
      */
-    MobSpellInstantaneous = "mobspellinstantaneous",
+    MobSpellInstantaneous = "minecraft:mobspellinstantaneous",
     /**
      * 	Eating, thrown eggs, splash potions, eyes of ender, breaking tools.
      */
-    IconCrack = "iconcrack",
+    IconCrack = "minecraft:iconcrack",
     /**
      * Jumping slimes.
      */
-    Slime = "slime",
+    Slime = "minecraft:slime",
     /**
      * Thrown snowballs, creating withers, creating iron golems.
      */
-    SnowballPoof = "snowballpoof",
+    SnowballPoof = "minecraft:snowballpoof",
     /**
      * Fire, minecart with furnace, blazes, water flowing into lava, lava flowing into water.
      */
-    LargeSmoke = "largesmoke",
+    LargeSmoke = "minecraft:largesmoke",
     /**
      * Lava
      */
-    Lava = "lava",
+    Lava = "minecraft:lava",
     /**
      * Burning entities, blazes for example.
      */
-    MobFlame = "mobflame",
+    MobFlame = "minecraft:mobflame",
     /**
      * Mycelium blocks.
      */
-    TownAura = "townaura",
+    TownAura = "minecraft:townaura",
     /**
      * Activated Conduits.
      */
-    Nautilus = "nautilus",
+    Nautilus = "minecraft:nautilus",
     /**
      * Emitted from note blocks and jukeboxes
      */
-    Note = "note",
+    Note = "minecraft:note",
     /**
      * Explosions, death of mobs, mobs spawned from a spawner, silverfish infesting blocks.
      */
-    Explode = "explode",
+    Explode = "minecraft:explode",
     /**
      * 	Nether portals, endermen, endermites, ender pearls, eyes of ender, ender chests, dragon eggs, teleporting from eating chorus fruits, end gateway portals.
      */
-    Portal = "portal",
+    Portal = "minecraft:portal",
     /**
      * Rain
      */
-    RainSplash = "rainsplash",
+    RainSplash = "minecraft:rainsplash",
     /**
      * 	Torches, primed TNT, droppers, dispensers, end portals, brewing stands, spawners, furnaces, ghast fireballs, wither skulls, taming, withers, lava (when raining), placing an eye of ender in an end portal frame, redstone torches burning out.
      */
-    Smoke = "smoke",
+    Smoke = "minecraft:smoke",
     /**
      * Entities in water, wolves shaking off after swimming, boats.
      */
-    WaterSplash = "watersplash",
+    WaterSplash = "minecraft:watersplash",
     /**
      * Produced by squids when attacked
      */
-    Ink = "ink",
+    Ink = "minecraft:ink",
     /**
      * ?
      */
-    Terrain = "terrain",
+    Terrain = "minecraft:terrain",
     /**
      * Activated totem of undying.
      */
-    Totem = "totem",
+    Totem = "minecraft:totem",
     /**
      * ?
      */
-    TrackingEmitter = "trackingemitter",
+    TrackingEmitter = "minecraft:trackingemitter",
     /**
      * Witches.
      */
-    WitchSpell = "witchspell"
+    WitchSpell = "minecraft:witchspell"
 }
