dff2e885e66ee9cd23345a0915fbfbf929db3e1e
Steven Blom <atomicblom@binaryvibrance.net>
2018-12-03 19:12:15 +1030

    Implement Lerna

---
+++ packages/common/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="./types/entity.d.ts" />
+/// <reference path="./types/event.d.ts" />
+/// <reference path="./types/particle_effects.d.ts" />
+/// <reference path="./types/system.d.ts" />
+/// <reference path="./types/view.d.ts" />
