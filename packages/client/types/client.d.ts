06b33831441cce6f80c7f5be3e8ddb687d80379e
Paul <44974737+pavll@users.noreply.github.com>
2020-08-04 17:08:54 +0200

    Update client.d.ts
    Renamed it so it does not conflict with 'namespace client' -> typescript naming conventions

--- packages/client/types/client.d.ts
+++ packages/client/types/client.d.ts
@@ -1,4 +1,4 @@
-declare const client: IClient;
+declare const CLIENT: IClient;
 
 declare interface IClient {
     registerSystem<TSystem extends IClientSystem<TSystem> = IVanillaClientSystem>(majorVersion: number, minorVersion: number): TSystem;
